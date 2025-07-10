ad95f803e73d22ac32a7f487642341ec54302917
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-07-10 09:12:18 +0200

    CS fixes

--- AllMySmsTransport.php
+++ AllMySmsTransport.php
@@ -43,7 +43,7 @@ final class AllMySmsTransport extends AbstractTransport
 
     public function __toString(): string
     {
-        return sprintf('allmysms://%s%s', $this->getEndpoint(), null !== $this->from ? '?from='.$this->from : '');
+        return \sprintf('allmysms://%s%s', $this->getEndpoint(), null !== $this->from ? '?from='.$this->from : '');
     }
 
     public function supports(MessageInterface $message): bool
@@ -62,7 +62,7 @@ final class AllMySmsTransport extends AbstractTransport
         $options['to'] = $message->getPhone();
         $options['text'] = $message->getSubject();
 
-        $endpoint = sprintf('https://%s/sms/send/', $this->getEndpoint());
+        $endpoint = \sprintf('https://%s/sms/send/', $this->getEndpoint());
         $response = $this->client->request('POST', $endpoint, [
             'auth_basic' => [$this->login, $this->apiKey],
             'json' => array_filter($options),
@@ -77,13 +77,13 @@ final class AllMySmsTransport extends AbstractTransport
         if (201 !== $statusCode) {
             $error = $response->toArray(false);
 
-            throw new TransportException(sprintf('Unable to send the SMS: "%s" (%s).', $error['description'], $error['code']), $response);
+            throw new TransportException(\sprintf('Unable to send the SMS: "%s" (%s).', $error['description'], $error['code']), $response);
         }
 
         $success = $response->toArray(false);
 
         if (false === isset($success['smsId'])) {
-            throw new TransportException(sprintf('Unable to send the SMS: "%s" (%s).', $success['description'], $success['code']), $response);
+            throw new TransportException(\sprintf('Unable to send the SMS: "%s" (%s).', $success['description'], $success['code']), $response);
         }
 
         $sentMessage = new SentMessage($message, (string) $this);
